314cd40792c0cdc7b0ac133be1e48ff4bcf12879
prabina123swain <122220753+prabina123swain@users.noreply.github.com>
2024-06-27 19:47:10 +0530

    Update mongoose.ts

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,39 +1,31 @@
-import mongoose, { Mongoose } from 'mongoose';
+import mongoose from 'mongoose';
 
-const MONGODB_URL = process.env.MONGODB_URL;
-
-interface MongooseConnection {
-  conn: Mongoose | null;
-  promise: Promise<Mongoose> | null;
-}
-
-let cached: MongooseConnection = (global as any).mongoose;
-
-if (!cached) {
-  cached = (global as any).mongoose = { 
-    conn: null, 
-    promise: null 
-  };
-}
-
-export const connectToDatabase = async (): Promise<Mongoose> => {
-  console.log("Inside connecting DB cached-> ", cached, "MongoDB URL->", MONGODB_URL);
-
-  if (cached.conn) return cached.conn;
-
-  if (!MONGODB_URL) throw new Error('Missing MONGODB_URL');
-
-  cached.promise = cached.promise || mongoose.connect(MONGODB_URL).then((mongooseInstance) => {
-    console.log("Database connected successfully");
-    return mongooseInstance;
-  }).catch((err) => {
-    console.log("Error in connecting DB: ", err.message);
-    process.exit(1);
-  });
-
-  console.log("Before cached =", cached);
-  cached.conn = await cached.promise;
-  console.log("After cached =", cached);
-  
-  return cached.conn;
+type ConnectionObject = {
+  isConnected?: number;
 };
+
+const connection: ConnectionObject = {};
+
+async function dbConnect(): Promise<void> {
+  // Check if we have a connection to the database or if it's currently connecting
+  if (connection.isConnected) {
+    console.log('Already connected to the database');
+    return;
+  }
+
+  try {
+    // Attempt to connect to the database
+    const db = await mongoose.connect(process.env.MONGODB_URL || '', {});
+
+    connection.isConnected = db.connections[0].readyState;
+
+    console.log('Database connected successfully');
+  } catch (error) {
+    console.error('Database connection failed:', error);
+
+    // Graceful exit in case of a connection error
+    process.exit(1);
+  }
+}
+
+export default dbConnect;
