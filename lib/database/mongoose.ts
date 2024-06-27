5e8ad505c394a3ce57af9ddfc71f27a9b61c28ea
prabina123swain <122220753+prabina123swain@users.noreply.github.com>
2024-06-27 19:56:47 +0530

    Update mongoose.ts

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,41 +1,35 @@
 import mongoose, { Mongoose } from "mongoose";
 
-const MONGODB_URL = process.env.MONGODB_URL;
+const MONGODB_URL = process.env.MONGODB_URL!;
 
-interface MongooseConnection {
-    conn: Mongoose | null;
-    promise: Promise<Mongoose> | null;
+interface MongooseConn {
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
 }
 
-// Initialize cached as a global variable if it doesn't exist
-let cached: MongooseConnection = (global as any).mongoose || { conn: null, promise: null };
+let cached: MongooseConn = (global as any).mongoose;
 
-// Save cached to the global object if it's not already there
-(global as any).mongoose = cached;
+if (!cached) {
+  cached = (global as any).mongoose = {
+    conn: null,
+    promise: null,
+  };
+}
 
-export const connectToDatabase = async (): Promise<Mongoose | null> => {
-    // Return existing connection if it's already established
-    if (cached.conn) return cached.conn;
+export const connectToDatabase = async () => {
+  if (cached.conn) return cached.conn;
 
-    // Throw an error if the MongoDB URL is missing
-    if (!MONGODB_URL) throw new Error("Missing MongoDB URL");
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGODB_URL, {
+      dbName: "clerk-next14-db",
+      bufferCommands: false,
+      connectTimeoutMS: 30000,
+    });
+  console.log("Before db connection",cached);
+  cached.conn = await cached.promise;
+  console.log("After db connection",cached);
 
-    // If there is no existing promise, create a new connection promise
-    if (!cached.promise) {
-        cached.promise = mongoose.connect(MONGODB_URL, {
-            dbName: "Imaginify",
-            bufferCommands: false,
-        }).then((mongooseInstance) => {
-            cached.conn = mongooseInstance;
-            return mongooseInstance;
-        }).catch((error) => {
-            cached.conn = null;
-            cached.promise = null;
-            throw error;
-        });
-    }
 
-    // Await the promise to get the connection
-    cached.conn = await cached.promise;
-    return cached.conn;
+  return cached.conn;
 };
