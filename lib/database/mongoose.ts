a4e5694d546dbcf7fe18601eb67e5665cdfdc887
prabina123swain <122220753+prabina123swain@users.noreply.github.com>
2024-06-27 19:28:10 +0530

    Update mongoose.ts

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -7,31 +7,33 @@ interface MongooseConnection {
   promise: Promise<Mongoose> | null;
 }
 
-let cached: MongooseConnection = (global as any).mongoose
+let cached: MongooseConnection = (global as any).mongoose;
 
-if(!cached) {
+if (!cached) {
   cached = (global as any).mongoose = { 
-    conn: null, promise: null 
-  }
+    conn: null, 
+    promise: null 
+  };
 }
 
-export const connectToDatabase = async () => {
-  console.log("inside connecting DB cached-> ",cached,"mongoDB Url->",MONGODB_URL);
-  if(cached.conn) return cached.conn;
+export const connectToDatabase = async (): Promise<Mongoose> => {
+  console.log("Inside connecting DB cached-> ", cached, "MongoDB URL->", MONGODB_URL);
 
-  if(!MONGODB_URL) throw new Error('Missing MONGODB_URL');
+  if (cached.conn) return cached.conn;
 
-  cached.promise = 
-    cached.promise || 
-   .then(()=>{
-    console.log("DataBase connected successfully");
-  })
-  .catch((err)=>{
-    console.log("error in connecting db: ",err.message);
+  if (!MONGODB_URL) throw new Error('Missing MONGODB_URL');
+
+  cached.promise = cached.promise || mongoose.connect(MONGODB_URL).then((mongooseInstance) => {
+    console.log("Database connected successfully");
+    return mongooseInstance;
+  }).catch((err) => {
+    console.log("Error in connecting DB: ", err.message);
     process.exit(1);
-  })
-  console.log("Before cached =",cached);
+  });
+
+  console.log("Before cached =", cached);
   cached.conn = await cached.promise;
-console.log("After cached =",cached);
+  console.log("After cached =", cached);
+  
   return cached.conn;
-}
+};
