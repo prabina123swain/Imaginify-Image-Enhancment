c6db21304bebbe3b12bd5e0dda32a62e4559b79f
prabina <swainprabina2001@gmail.com>
2024-06-28 00:28:43 +0530

    update model

--- lib/models/user.model.ts
+++ lib/models/user.model.ts
@@ -1,58 +1,41 @@
-import { Schema, model, models, Document, Model } from "mongoose";
+import { Schema, model, models } from "mongoose";
 
-// Define the TypeScript interface for the User schema
-interface IUser extends Document {
-    clerkId: string;
-    firstName: string;
-    lastName: string;
-    email: string;
-    username: string;
-    photo?: string;
-    planId: Number;
-    creditBalance: number;
-}
-
-// Define the User schema
-const UserSchema = new Schema<IUser>({
-    clerkId: {
-        type: String,
-        required: true,
-        unique: true,
-    },   
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    firstName: {
-        type: String,
-        required: true,
-    },
-    lastName: {
-        type: String,
-        required: true,
-    },
-    photo: {
-        type: String,
-    },
-    planId: {
-        type: Number,
-        default:1
-    },
-    creditBalance: {
-        type: Number,
-        default: 10,
-    },
-}, {
-    timestamps: true, // Automatically manage createdAt and updatedAt fields
+const UserSchema = new Schema({
+  clerkId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  photo: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  planId: {
+    type: Number,
+    default: 1,
+  },
+  creditBalance: {
+    type: Number,
+    default: 10,
+  },
 });
 
-// Create the User model
-const User: Model<IUser> = models.User || model<IUser>('User', UserSchema);
+const User = models?.User || model("User", UserSchema);
 
 export default User;
